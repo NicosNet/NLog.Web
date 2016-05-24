37067633b9eb519915ca8a28aa50d30784a4afe0
Julian Verdurmen <304NotModified@users.noreply.github.com>
2016-05-24 23:32:33 +0200

    move asp.net helpers to this lib

--- NLog.Web.AspNetCore/AspNetExtensions.cs
+++ NLog.Web.AspNetCore/AspNetExtensions.cs
@@ -1,8 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Config;
+using NLog.Extensions.Logging;
 using NLog.Web.Internal;
 
 namespace NLog.Web
@@ -20,6 +26,69 @@ namespace NLog.Web
         public static void AddNLogWeb(this IApplicationBuilder app)
         {
             ServiceLocator.ServiceProvider = app.ApplicationServices;
+        }
+
+        /// <summary>
+        /// Enable NLog as logging provider in ASP.NET Core.
+        /// </summary>
+        /// <param name="factory"></param>
+        /// <returns></returns>
+        public static ILoggerFactory AddNLog(this ILoggerFactory factory)
+        {
+            //ignore this
+            LogManager.AddHiddenAssembly(typeof(AspNetExtensions).GetTypeInfo().Assembly);
+
+            using (var provider = new NLogLoggerProvider())
+            {
+                factory.AddProvider(provider);
+            }
+            return factory;
+        }
+
+        /// <summary>
+        /// Apply NLog configuration from XML config.
+        /// </summary>
+        /// <param name="env"></param>
+        /// <param name="configFileRelativePath">relative path to NLog configuration file.</param>
+        /// <returns>Current configuration for chaining.</returns>
+        public static LoggingConfiguration ConfigureNLog(this IHostingEnvironment env, string configFileRelativePath)
+        {
+            var fileName = Path.Combine(Directory.GetParent(env.WebRootPath).FullName, configFileRelativePath);
+            return ConfigureNLog(fileName);
+        }
+
+        /// <summary>
+        /// Apply NLog configuration from config object.
+        /// </summary>
+        /// <param name="env"></param>
+        /// <param name="config">New NLog config.</param>
+        /// <returns>Current configuration for chaining.</returns>
+        public static LoggingConfiguration ConfigureNLog(this IHostingEnvironment env, LoggingConfiguration config)
+        {
+            LogManager.Configuration = config;
+
+            return config;
+        }
+
+        /// <summary>
+        /// Start NLog configuration.
+        /// </summary>
+        /// <param name="env"></param>
+        /// <returns>Current configuration for chaining.</returns>
+        public static LoggingConfiguration ConfigureNLog(this IHostingEnvironment env)
+        {
+            return LogManager.Configuration;
+        }
+
+        /// <summary>
+        /// Apply NLog configuration from XML config.
+        /// </summary>
+        /// <param name="fileName">absolute path  NLog configuration file.</param>
+        private static LoggingConfiguration ConfigureNLog(string fileName)
+        {
+            var configuration = new XmlLoggingConfiguration(fileName, true);
+            LogManager.Configuration = configuration;
+            return configuration;
         }
 
     }
